d6a08d128336db22a9f13a73bbfea9764eec7397
Sohail Zafar <you@example.com>
2019-03-29 21:39:48 -0700

    incomplete

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,5 @@
+//  Assignment: Friend Finder 
+//  Programmer: Sohail Zafar 
+//  File: apiRoutes.js
+//  This application asks users questions and matches users with other users who answered the survey similarly.
+//  This file is incomplete. Will complete it soon.
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //  Assignment: Friend Finder 
 //  Programmer: Sohail Zafar 
-//  File: htmlRoutes.js
+//  File: index.js
 //  This application asks users questions and matches users with other users who answered the survey similarly.
-// This file is incomplete. Will complete it soon. 
+// This file is incomplete. Will complete it soon.
+
+  module.exports = friends;
+
+  var oldFriend = [];
+  var newFriend = {}
